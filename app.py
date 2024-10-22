b116c0aacf20267b8b27903f12c7da202fcd66e3
Dymovv <69103637+Dymovv@users.noreply.github.com>
2024-10-22 22:23:35 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import random
+
+ex = [exnum for exnum in range(100,1000) if str(exnum).count('0') > 0] # добавление в спсисок чисел-исключений (в данном случае это числа от 100 до 999, имеющие 0 в составе)
+compnumber = random.choice([num for num in range(100,10000) if len(set(str(num))) == len(str(num)) and num not in ex])  # число, загаданное компьютером (с ограничениями)
+
+if compnumber < 1000:
+  compnumber = '0'+str(compnumber)        # добавление 0 в начало 3-значных чисел
+
+flag = True
+
+while flag:
+  player_move = int(input('Введите число: '))
+  if (len(set(str((player_move)))) < len(str(player_move))) or (len(str(player_move)) != 4):        # проверка корректности ввода
+    print('Некорректный формат ввода!')
+    continueЫЫ
+  
+  bulls = 0
+  counter = 0
+  for num in str(compnumber):                                     # проверка быков
+    if str(player_move)[counter] == num:
+      bulls += 1
+    counter += 1
+
+  counter = 0
+  cows = 0
+  for num in str(compnumber):                                               # проверка коров
+    if str(player_move)[counter] != num and num in str(player_move):
+      cows += 1
+    counter += 1
+
+  print('Коровы:', cows, 'Быки', bulls)
+  print(compnumber)
+  if player_move == compnumber:
+    print('Вы победили!')
+    break
+  
+  
+
+  
